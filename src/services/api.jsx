6ff4c3e31192928cfc01e033a32a1328a24b498b
OlenaYarotska <olena.zhurba@gmail.com>
2022-02-12 18:18:16 +0200

    add fixes

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,14 +2,14 @@ import axios from "axios";
 const KEY = "24368394-ccc0003f8191eae78e1f7d910";
 const BaseUrl = "https://pixabay.com/api/";
 
-async function fetchImages(search, page) {
-  const { response } = await axios.get(
+const fetchImages = async (search, page) => {
+  const { data } = await axios.get(
     `${BaseUrl}?q=${search}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
 
   // const fetchedImages = await response;
 
   // return fetchedImages.data;
-  return response.hits;
-}
+  return data.hits;
+};
 export default fetchImages;
